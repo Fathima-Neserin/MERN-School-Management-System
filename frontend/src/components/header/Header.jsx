794ac510071106f4b0c223f14a2e03c424b3477b
fathima <fathimanezrin2001@gmail.com>
2024-11-12 13:21:00 +0530

    gap fixes

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
     }
   },[])
   return (
-    <div className="bg-indigo-950 shadow-lg mr-[8px] h-15">
-      <header className="w-screen max-w-6xl mx-5 flex justify-between items-center px-4 py-2">
+    <div className="bg-indigo-950 shadow-lg  h-15">
+      <header className="w-screen  mx-5 flex justify-between items-center px-4 py-2">
         <h1 className="text-2xl font-bold text-white">
         {role === 'Admin' ? 'Admin Dashboard' :
          role === 'Staff' ? 'Staff Dashboard' :
